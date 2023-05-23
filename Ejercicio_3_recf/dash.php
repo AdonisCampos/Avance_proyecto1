877d2be50c1aa8e9ded999eec2a335b8e5a51d9f
AdonisCampos <127464436+AdonisCampos@users.noreply.github.com>
2023-05-23 11:10:40 -0600

    Add files via upload

---
+++ Ejercicio_3_recf/dash.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=ç, initial-scale=1.0">
+    <title>Panel de Control</title>
+</head>
+<body>
+<h2>Bienvenido</h2>
+<p>Has iniciado sesión con éxito.</p>
+</body>
+</html>
+
